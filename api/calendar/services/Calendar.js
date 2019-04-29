666ad86cae995d56ebfbea1085e4bc84a3e02f1d
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-04-29 12:44:02 -0400

    feat: restrict user scope in calendar

--- api/calendar/services/Calendar.js
+++ api/calendar/services/Calendar.js
@@ -21,6 +21,45 @@ module.exports = {
    * @return {Promise}
    */
 
+  fetchAllOfUser: (params, user_id) => {
+    // Convert `params` object to filters compatible with Bookshelf.
+    const filters = strapi.utils.models.convertParams('calendar', params);
+    // Select field to populate.
+    const populate = Calendar.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    return Calendar.query(function(qb) {
+      _.forEach(filters.where, (where, key) => {
+        if (_.isArray(where.value) && where.symbol !== 'IN' && where.symbol !== 'NOT IN') {
+          for (const value in where.value) {
+            qb[value ? 'where' : 'orWhere'](key, where.symbol, where.value[value])
+          }
+        } else {
+          qb.where(key, where.symbol, where.value);
+        }
+      });
+
+      if (filters.sort) {
+        qb.orderBy(filters.sort.key, filters.sort.order);
+      }
+
+      qb.offset(filters.start);
+      qb.limit(filters.limit);
+      qb.where({
+          owner: user_id
+      })
+    }).fetchAll({
+      withRelated: filters.populate || populate
+    });
+  },
+
+  /**
+   * Promise to fetch all calendars.
+   *
+   * @return {Promise}
+   */
+
   fetchAll: (params) => {
     // Convert `params` object to filters compatible with Bookshelf.
     const filters = strapi.utils.models.convertParams('calendar', params);
@@ -57,6 +96,26 @@ module.exports = {
    * @return {Promise}
    */
 
+  fetchOfUser: async (params, user_id) => {
+    // Select field to populate.
+    const populate = Calendar.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    const entry = await Calendar.forge(_.pick(params, 'id')).fetch({
+      withRelated: populate
+    });
+
+    if (entry && entry.attributes.owner !== user_id) return null;
+
+    return entry;
+  },
+  /**
+   * Promise to fetch a/an calendar.
+   *
+   * @return {Promise}
+   */
+
   fetch: (params) => {
     // Select field to populate.
     const populate = Calendar.associations
@@ -66,6 +125,32 @@ module.exports = {
     return Calendar.forge(_.pick(params, 'id')).fetch({
       withRelated: populate
     });
+  },
+
+  /**
+   * Promise to count a/an calendar.
+   *
+   * @return {Promise}
+   */
+
+  countOfUser: (params, user_id) => {
+    // Convert `params` object to filters compatible with Bookshelf.
+    const filters = strapi.utils.models.convertParams('calendar', params);
+
+    return Calendar.query(function(qb) {
+      qb.where({
+        owner: user_id
+      });
+      _.forEach(filters.where, (where, key) => {
+        if (_.isArray(where.value)) {
+          for (const value in where.value) {
+            qb[value ? 'where' : 'orWhere'](key, where.symbol, where.value[value]);
+          }
+        } else {
+          qb.where(key, where.symbol, where.value);
+        }
+      });
+    }).count();
   },
 
   /**
@@ -115,6 +200,34 @@ module.exports = {
    * @return {Promise}
    */
 
+  editOfUser: async (params, values, user_id) => {
+    // Extract values related to relational data.
+    const populate = Calendar.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    const check_entry = await Calendar.forge(_.pick(params, 'id')).fetch({
+      withRelated: populate
+    });
+
+    if (!check_entry || !check_entry.attributes || check_entry.attributes.owner !== user_id) return null;
+
+    const relations = _.pick(values, Calendar.associations.map(ast => ast.alias));
+    const data = _.omit(values, Calendar.associations.map(ast => ast.alias));
+
+    // Create entry with no-relational data.
+    const entry = Calendar.forge(params).save(data);
+
+    // Create relational data and return the entry.
+    return Calendar.updateRelations(Object.assign(params, { values: relations }));
+  },
+
+  /**
+   * Promise to edit a/an calendar.
+   *
+   * @return {Promise}
+   */
+
   edit: async (params, values) => {
     // Extract values related to relational data.
     const relations = _.pick(values, Calendar.associations.map(ast => ast.alias));
@@ -126,6 +239,49 @@ module.exports = {
     // Create relational data and return the entry.
     return Calendar.updateRelations(Object.assign(params, { values: relations }));
   },
+
+  /**
+   * Promise to remove a/an calendar.
+   *
+   * @return {Promise}
+   */
+
+  removeOfUser: async (params, user_id) => {
+
+    // Extract values related to relational data.
+    const populate = Calendar.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    const check_entry = await Calendar.forge(_.pick(params, 'id')).fetch({
+      withRelated: populate
+    });
+
+    if (!check_entry || !check_entry.attributes || check_entry.attributes.owner !== user_id) return null;
+
+    params.values = {};
+    Calendar.associations.map(association => {
+      switch (association.nature) {
+        case 'oneWay':
+        case 'oneToOne':
+        case 'manyToOne':
+        case 'oneToManyMorph':
+          params.values[association.alias] = null;
+          break;
+        case 'oneToMany':
+        case 'manyToMany':
+        case 'manyToManyMorph':
+          params.values[association.alias] = [];
+          break;
+        default:
+      }
+    });
+
+    await Calendar.updateRelations(params);
+
+    return Calendar.forge(params).destroy();
+  },
+
 
   /**
    * Promise to remove a/an calendar.
@@ -155,6 +311,95 @@ module.exports = {
     await Calendar.updateRelations(params);
 
     return Calendar.forge(params).destroy();
+  },
+
+  /**
+   * Promise to search a/an calendar.
+   *
+   * @return {Promise}
+   */
+
+  searchOfUser: async (params, user_id) => {
+    // Convert `params` object to filters compatible with Bookshelf.
+    const filters = strapi.utils.models.convertParams('calendar', params);
+    // Select field to populate.
+    const populate = Calendar.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    const associations = Calendar.associations.map(x => x.alias);
+    const searchText = Object.keys(Calendar._attributes)
+        .filter(attribute => attribute !== Calendar.primaryKey && !associations.includes(attribute))
+        .filter(attribute => ['string', 'text'].includes(Calendar._attributes[attribute].type));
+
+    const searchNoText = Object.keys(Calendar._attributes)
+        .filter(attribute => attribute !== Calendar.primaryKey && !associations.includes(attribute))
+        .filter(attribute => !['string', 'text', 'boolean', 'integer', 'decimal', 'float'].includes(Calendar._attributes[attribute].type));
+
+    const searchInt = Object.keys(Calendar._attributes)
+        .filter(attribute => attribute !== Calendar.primaryKey && !associations.includes(attribute))
+        .filter(attribute => ['integer', 'decimal', 'float'].includes(Calendar._attributes[attribute].type));
+
+    const searchBool = Object.keys(Calendar._attributes)
+        .filter(attribute => attribute !== Calendar.primaryKey && !associations.includes(attribute))
+        .filter(attribute => ['boolean'].includes(Calendar._attributes[attribute].type));
+
+    const query = (params._q || '').replace(/[^a-zA-Z0-9.-\s]+/g, '');
+
+    return Calendar.query(qb => {
+      // Search in columns which are not text value.
+      searchNoText.forEach(attribute => {
+        qb.orWhereRaw(`LOWER(${attribute}) LIKE '%${_.toLower(query)}%'`);
+      });
+
+      if (!_.isNaN(_.toNumber(query))) {
+        searchInt.forEach(attribute => {
+          qb.orWhereRaw(`${attribute} = ${_.toNumber(query)}`);
+        });
+      }
+
+      if (query === 'true' || query === 'false') {
+        searchBool.forEach(attribute => {
+          qb.orWhereRaw(`${attribute} = ${_.toNumber(query === 'true')}`);
+        });
+      }
+
+      // Search in columns with text using index.
+      switch (Calendar.client) {
+        case 'mysql':
+          qb.orWhereRaw(`MATCH(${searchText.join(',')}) AGAINST(? IN BOOLEAN MODE)`, `*${query}*`);
+          break;
+        case 'pg': {
+          const searchQuery = searchText.map(attribute =>
+              _.toLower(attribute) === attribute
+                  ? `to_tsvector(${attribute})`
+                  : `to_tsvector('${attribute}')`
+          );
+
+          qb.orWhereRaw(`${searchQuery.join(' || ')} @@ to_tsquery(?)`, query);
+          break;
+        }
+      }
+
+      if (filters.sort) {
+        qb.orderBy(filters.sort.key, filters.sort.order);
+      }
+
+      if (filters.skip) {
+        qb.offset(_.toNumber(filters.skip));
+      }
+
+      if (filters.limit) {
+        qb.limit(_.toNumber(filters.limit));
+      }
+
+      qb.where({
+        owner: user_id
+      });
+
+    }).fetchAll({
+      width: populate
+    });
   },
 
   /**
@@ -240,4 +485,5 @@ module.exports = {
       width: populate
     });
   }
+
 };
