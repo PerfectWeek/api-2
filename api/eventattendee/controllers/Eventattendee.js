68cc08fdeb26064a03b15933be30614f79d1705a
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-05-11 17:20:59 -0400

    feat: restrict user scope in eventattendee

--- api/eventattendee/controllers/Eventattendee.js
+++ api/eventattendee/controllers/Eventattendee.js
@@ -16,9 +16,9 @@ module.exports = {
 
   find: async (ctx) => {
     if (ctx.query._q) {
-      return strapi.services.eventattendee.search(ctx.query);
+      return strapi.services.eventattendee.searchOfUser(ctx.query, ctx.state.user.id);
     } else {
-      return strapi.services.eventattendee.fetchAll(ctx.query);
+      return strapi.services.eventattendee.fetchAllOfUser(ctx.query, ctx.state.user.id);
     }
   },
 
@@ -29,7 +29,7 @@ module.exports = {
    */
 
   findOne: async (ctx) => {
-    return strapi.services.eventattendee.fetch(ctx.params);
+    return strapi.services.eventattendee.fetchOfUser(ctx.params, ctx.state.user.id);
   },
 
   /**
@@ -39,7 +39,7 @@ module.exports = {
    */
 
   count: async (ctx) => {
-    return strapi.services.eventattendee.count(ctx.query);
+    return strapi.services.eventattendee.countOfUser(ctx.query, ctx.state.user.id);
   },
 
   /**
@@ -59,7 +59,7 @@ module.exports = {
    */
 
   update: async (ctx, next) => {
-    return strapi.services.eventattendee.edit(ctx.params, ctx.request.body) ;
+    return strapi.services.eventattendee.editOfUser(ctx.params, ctx.request.body, ctx.state.user.id);
   },
 
   /**
@@ -69,6 +69,6 @@ module.exports = {
    */
 
   destroy: async (ctx, next) => {
-    return strapi.services.eventattendee.remove(ctx.params);
+    return strapi.services.eventattendee.removeOfUser(ctx.params, ctx.state.user.id);
   }
 };
--- api/eventattendee/services/Eventattendee.js
+++ api/eventattendee/services/Eventattendee.js
@@ -21,13 +21,52 @@ module.exports = {
    * @return {Promise}
    */
 
+  fetchAllOfUser: (params, user_id) => {
+    // Convert `params` object to filters compatible with Bookshelf.
+    const filters = strapi.utils.models.convertParams('eventattendee', params);
+    // Select field to populate.
+    const populate = Eventattendee.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    return Eventattendee.query(function(qb) {
+      _.forEach(filters.where, (where, key) => {
+        if (_.isArray(where.value) && where.symbol !== 'IN' && where.symbol !== 'NOT IN') {
+          for (const value in where.value) {
+            qb[value ? 'where' : 'orWhere'](key, where.symbol, where.value[value])
+          }
+        } else {
+          qb.where(key, where.symbol, where.value);
+        }
+      });
+
+      if (filters.sort) {
+        qb.orderBy(filters.sort.key, filters.sort.order);
+      }
+
+      qb.offset(filters.start);
+      qb.limit(filters.limit);
+      qb.where({
+        user: user_id
+      })
+    }).fetchAll({
+      withRelated: filters.populate || populate
+    })
+  },
+
+  /**
+   * Promise to fetch all eventattendees.
+   *
+   * @return {Promise}
+   */
+
   fetchAll: (params) => {
     // Convert `params` object to filters compatible with Bookshelf.
     const filters = strapi.utils.models.convertParams('eventattendee', params);
     // Select field to populate.
     const populate = Eventattendee.associations
-      .filter(ast => ast.autoPopulate !== false)
-      .map(ast => ast.alias);
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
 
     return Eventattendee.query(function(qb) {
       _.forEach(filters.where, (where, key) => {
@@ -57,15 +96,62 @@ module.exports = {
    * @return {Promise}
    */
 
+  fetchOfUser: async (params, user_id) => {
+    // Select field to populate.
+    const populate = Eventattendee.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    const entry = await Eventattendee.forge(_.pick(params, 'id')).fetch({
+      withRelated: populate
+    });
+
+    if (entry && entry.attributes.user !== user_id) return null;
+
+    return entry;
+  },
+
+  /**
+   * Promise to fetch a/an eventattendee.
+   *
+   * @return {Promise}
+   */
+
   fetch: (params) => {
     // Select field to populate.
     const populate = Eventattendee.associations
-      .filter(ast => ast.autoPopulate !== false)
-      .map(ast => ast.alias);
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
 
     return Eventattendee.forge(_.pick(params, 'id')).fetch({
       withRelated: populate
     });
+  },
+
+  /**
+   * Promise to count a/an eventattendee.
+   *
+   * @return {Promise}
+   */
+
+  countOfUser: (params, user_id) => {
+    // Convert `params` object to filters compatible with Bookshelf.
+    const filters = strapi.utils.models.convertParams('eventattendee', params);
+
+    return Eventattendee.query(function(qb) {
+      qb.where({
+        user: user_id
+      });
+      _.forEach(filters.where, (where, key) => {
+        if (_.isArray(where.value)) {
+          for (const value in where.value) {
+            qb[value ? 'where' : 'orWhere'](key, where.symbol, where.value[value]);
+          }
+        } else {
+          qb.where(key, where.symbol, where.value);
+        }
+      });
+    }).count();
   },
 
   /**
@@ -115,6 +201,34 @@ module.exports = {
    * @return {Promise}
    */
 
+  editOfUser: async (params, values, user_id) => {
+    const populate = Eventattendee.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    const check_entry = await Eventattendee.forge(_.pick(params, 'id')).fetch({
+      withRelated: populate
+    });
+
+    if (!check_entry || !check_entry.attributes || check_entry.attributes.user !== user_id) return null;
+
+    // Extract values related to relational data.
+    const relations = _.pick(values, Eventattendee.associations.map(ast => ast.alias));
+    const data = _.omit(values, Eventattendee.associations.map(ast => ast.alias));
+
+    // Create entry with no-relational data.
+    const entry = Eventattendee.forge(params).save(data);
+
+    // Create relational data and return the entry.
+    return Eventattendee.updateRelations(Object.assign(params, { values: relations }));
+  },
+
+  /**
+   * Promise to edit a/an eventattendee.
+   *
+   * @return {Promise}
+   */
+
   edit: async (params, values) => {
     // Extract values related to relational data.
     const relations = _.pick(values, Eventattendee.associations.map(ast => ast.alias));
@@ -125,6 +239,46 @@ module.exports = {
 
     // Create relational data and return the entry.
     return Eventattendee.updateRelations(Object.assign(params, { values: relations }));
+  },
+
+  /**
+   * Promise to remove a/an eventattendee.
+   *
+   * @return {Promise}
+   */
+
+  removeOfUser: async (params) => {
+    const populate = Eventattendee.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    const check_entry = await Eventattendee.forge(_.pick(params, 'id')).fetch({
+      withRelated: populate
+    });
+
+    if (!check_entry || !check_entry.attributes || check_entry.attributes.user !== user_id) return null;
+
+    params.values = {};
+    Eventattendee.associations.map(association => {
+      switch (association.nature) {
+        case 'oneWay':
+        case 'oneToOne':
+        case 'manyToOne':
+        case 'oneToManyMorph':
+          params.values[association.alias] = null;
+          break;
+        case 'oneToMany':
+        case 'manyToMany':
+        case 'manyToManyMorph':
+          params.values[association.alias] = [];
+          break;
+        default:
+      }
+    });
+
+    await Eventattendee.updateRelations(params);
+
+    return Eventattendee.forge(params).destroy();
   },
 
   /**
@@ -163,30 +317,30 @@ module.exports = {
    * @return {Promise}
    */
 
-  search: async (params) => {
+  searchOfUser: async (params, user_id) => {
     // Convert `params` object to filters compatible with Bookshelf.
     const filters = strapi.utils.models.convertParams('eventattendee', params);
     // Select field to populate.
     const populate = Eventattendee.associations
-      .filter(ast => ast.autoPopulate !== false)
-      .map(ast => ast.alias);
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
 
     const associations = Eventattendee.associations.map(x => x.alias);
     const searchText = Object.keys(Eventattendee._attributes)
-      .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
-      .filter(attribute => ['string', 'text'].includes(Eventattendee._attributes[attribute].type));
+        .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
+        .filter(attribute => ['string', 'text'].includes(Eventattendee._attributes[attribute].type));
 
     const searchNoText = Object.keys(Eventattendee._attributes)
-      .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
-      .filter(attribute => !['string', 'text', 'boolean', 'integer', 'decimal', 'float'].includes(Eventattendee._attributes[attribute].type));
+        .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
+        .filter(attribute => !['string', 'text', 'boolean', 'integer', 'decimal', 'float'].includes(Eventattendee._attributes[attribute].type));
 
     const searchInt = Object.keys(Eventattendee._attributes)
-      .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
-      .filter(attribute => ['integer', 'decimal', 'float'].includes(Eventattendee._attributes[attribute].type));
+        .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
+        .filter(attribute => ['integer', 'decimal', 'float'].includes(Eventattendee._attributes[attribute].type));
 
     const searchBool = Object.keys(Eventattendee._attributes)
-      .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
-      .filter(attribute => ['boolean'].includes(Eventattendee._attributes[attribute].type));
+        .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
+        .filter(attribute => ['boolean'].includes(Eventattendee._attributes[attribute].type));
 
     const query = (params._q || '').replace(/[^a-zA-Z0-9.-\s]+/g, '');
 
@@ -215,9 +369,9 @@ module.exports = {
           break;
         case 'pg': {
           const searchQuery = searchText.map(attribute =>
-            _.toLower(attribute) === attribute
-              ? `to_tsvector(${attribute})`
-              : `to_tsvector('${attribute}')`
+              _.toLower(attribute) === attribute
+                  ? `to_tsvector(${attribute})`
+                  : `to_tsvector('${attribute}')`
           );
 
           qb.orWhereRaw(`${searchQuery.join(' || ')} @@ to_tsquery(?)`, query);
@@ -236,6 +390,95 @@ module.exports = {
       if (filters.limit) {
         qb.limit(_.toNumber(filters.limit));
       }
+
+      qb.where({
+        user: user_id
+      })
+    }).fetchAll({
+      width: populate
+    });
+  },
+
+  /**
+   * Promise to search a/an eventattendee.
+   *
+   * @return {Promise}
+   */
+
+  search: async (params) => {
+    // Convert `params` object to filters compatible with Bookshelf.
+    const filters = strapi.utils.models.convertParams('eventattendee', params);
+    // Select field to populate.
+    const populate = Eventattendee.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias);
+
+    const associations = Eventattendee.associations.map(x => x.alias);
+    const searchText = Object.keys(Eventattendee._attributes)
+        .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
+        .filter(attribute => ['string', 'text'].includes(Eventattendee._attributes[attribute].type));
+
+    const searchNoText = Object.keys(Eventattendee._attributes)
+        .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
+        .filter(attribute => !['string', 'text', 'boolean', 'integer', 'decimal', 'float'].includes(Eventattendee._attributes[attribute].type));
+
+    const searchInt = Object.keys(Eventattendee._attributes)
+        .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
+        .filter(attribute => ['integer', 'decimal', 'float'].includes(Eventattendee._attributes[attribute].type));
+
+    const searchBool = Object.keys(Eventattendee._attributes)
+        .filter(attribute => attribute !== Eventattendee.primaryKey && !associations.includes(attribute))
+        .filter(attribute => ['boolean'].includes(Eventattendee._attributes[attribute].type));
+
+    const query = (params._q || '').replace(/[^a-zA-Z0-9.-\s]+/g, '');
+
+    return Eventattendee.query(qb => {
+      // Search in columns which are not text value.
+      searchNoText.forEach(attribute => {
+        qb.orWhereRaw(`LOWER(${attribute}) LIKE '%${_.toLower(query)}%'`);
+      });
+
+      if (!_.isNaN(_.toNumber(query))) {
+        searchInt.forEach(attribute => {
+          qb.orWhereRaw(`${attribute} = ${_.toNumber(query)}`);
+        });
+      }
+
+      if (query === 'true' || query === 'false') {
+        searchBool.forEach(attribute => {
+          qb.orWhereRaw(`${attribute} = ${_.toNumber(query === 'true')}`);
+        });
+      }
+
+      // Search in columns with text using index.
+      switch (Eventattendee.client) {
+        case 'mysql':
+          qb.orWhereRaw(`MATCH(${searchText.join(',')}) AGAINST(? IN BOOLEAN MODE)`, `*${query}*`);
+          break;
+        case 'pg': {
+          const searchQuery = searchText.map(attribute =>
+              _.toLower(attribute) === attribute
+                  ? `to_tsvector(${attribute})`
+                  : `to_tsvector('${attribute}')`
+          );
+
+          qb.orWhereRaw(`${searchQuery.join(' || ')} @@ to_tsquery(?)`, query);
+          break;
+        }
+      }
+
+      if (filters.sort) {
+        qb.orderBy(filters.sort.key, filters.sort.order);
+      }
+
+      if (filters.skip) {
+        qb.offset(_.toNumber(filters.skip));
+      }
+
+      if (filters.limit) {
+        qb.limit(_.toNumber(filters.limit));
+      }
+
     }).fetchAll({
       width: populate
     });
